2d5b15eae2f2310b193f09a6cf7d4368f40f441b
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2023-04-10 23:58:07 +0200

    Add modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,17 @@ import * as utils from './utils.js';
 import * as json from '../data.json' assert { type: 'json' };
 import CommentsList from "./Classes/CommentsList.js";
 import { Form } from './Classes/Form.js';
+import Modal from './Classes/Modal.js';
 
 const data = utils.getDataJSON(json);
 const USER_NAME = data.currentUser.username;
 const USER_AVATAR = data.currentUser.image.png;
 const commentsSection = document.querySelector('#comments');
 const addCommentSection = document.querySelector('#add-comment')
+
 const comments = new CommentsList(USER_NAME, USER_AVATAR)
+
+new Modal(commentsSection)
 
 new Form(addCommentSection, true)
 
