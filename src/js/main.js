86d82407d7e1b3704be5a51acdd598066faf74fc
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2023-04-10 16:13:25 +0200

    Change names of user data variables

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,19 @@
 import * as utils from './utils.js';
 import * as json from '../data.json' assert { type: 'json' };
-import CommentsList from "./CommentsList.js";
-import Form from './Form.js';
-import Modal from "./Modal.js";
+import CommentsList from "./Classes/CommentsList.js";
+import Form from './Classes/Form.js';
 
 const data = utils.getDataJSON(json);
-const userName = data.currentUser.username;
-const userAvatar = data.currentUser.image.png;
+const USER_NAME = data.currentUser.username;
+const USER_AVATAR = data.currentUser.image.png;
 const commentsSection = document.querySelector('#comments');
 const addCommentSection = document.querySelector('#add-comment')
-const comments = new CommentsList(userName, userAvatar)
+const comments = new CommentsList(USER_NAME, USER_AVATAR)
 
-const form = new Form(addCommentSection, true)
-
-// const modal = new Modal(commentsSection)
+new Form(addCommentSection, true)
 
 comments.generateComments(data.comments, commentsSection)
 
 console.log(data)
 
-export { userName, userAvatar }
+export { USER_NAME, USER_AVATAR }
