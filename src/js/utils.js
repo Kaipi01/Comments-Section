6bc637a540f19ddacc17abf429efe853165dc352
Mateusz Filipiak <mateusz.filipiak01@gmail.com>
2023-05-16 13:45:53 +0200

    Add another helpful functions

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,15 @@
-let unusedNumberID = 5;
 const deleteEvent = new CustomEvent('custom:delete', {
     bubbles: true
 })
 const openModalEvent = new CustomEvent('custom:open-modal', {
     bubbles: true
 })
+const createCommentEvent = new CustomEvent('custom:create-comment')
 
-export { deleteEvent, openModalEvent }
+export { deleteEvent, openModalEvent, createCommentEvent }
 
 export function generateID() {
-    return unusedNumberID++
+    return parseInt(Math.ceil(Math.random() * Date.now()))
 }
 
 export function getDataJSON(json) {
@@ -18,27 +18,12 @@ export function getDataJSON(json) {
     return data.default
 }
 
-export function getLocalStorageData(key) {
-    const data = []
-    let i = 1
-
-    // while (localStorage.getItem(key + i)) {
-    //     data.push(
-    //         JSON.parse(localStorage.getItem(key + i))
-    //     )
-    //     i++
-    // }
-    return data
-}
-
 export function enableBtn(btn) {
     btn.removeAttribute('disabled');
-    btn.classList.remove('comment__btn-vote--clicked')
 }
 
 export function disableBtn(btn) {
     btn.setAttribute('disabled', true)
-    btn.classList.add('comment__btn-vote--clicked')
 }
 
 export function animate(element, animationClass, animationTime = 500) {
@@ -46,10 +31,27 @@ export function animate(element, animationClass, animationTime = 500) {
     setTimeout(() => element.classList.remove(animationClass), animationTime)
 }
 
-export function getFormatDate() {
-    const date = new Date();
-    const dateISOFormat = date.toISOString().slice(0,10)
-    const dateMnemonicFormat = 'Today'
-    
-    return {dateISOFormat, dateMnemonicFormat}
+export function isUniqueArrayOfObjects(array, key) {
+    let flag = true
+
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < array.length; j++) {
+            if (array[j][key] === array[i][key] && j !== i)
+                flag = false
+        }
+    }
+    return flag
+}
+
+export function findNestedObjectByKey(collection, key, value) {
+    for (let object of collection) {
+        for (let [k, v] of Object.entries(object)) {
+            if (k === key && v === value)
+                return object
+            if (Array.isArray(v)) {
+                object =  findNestedObjectByKey(v, key, value)
+                if (object) return object
+            }
+        }
+    }
 }
